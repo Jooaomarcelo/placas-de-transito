3065a6da7b43caabd3dcb68c765a4ae0c39a32c3
HenriUz <henriquezucato@gmail.com>
2025-07-03 19:55:53 -0300

    Added first classification method.

--- script.py
+++ script.py
@@ -240,6 +240,26 @@ def find(img_path, janela):
     # cv.imwrite(paths_concatenadas[2], grad_thresh)
     cv.imwrite(paths_concatenadas[3], output)
 
+def classificacao(vetor):
+    # Vetor: [circularidade, aspect radio, vertices, cor principal, cor secundaria].
+    
+    placa = 0   # Tipo da placa identificada, seguindo a lógica: [0: não é placa, 1: regulamentação, 2: advertência, 3: indicação, 4: sinalização temporária].
+
+    if vetor[3] in ["vermelho", "branco"] and vetor[4] in ["vermelho", "branco"]:
+        if vetor[0] >= 0.85 or (vetor[1] >= 0.75 and vetor[1] <= 1.25) or (vetor[2] == 3 or vetor[2] == 8):
+            placa = 1
+    elif vetor[3] == "amarelo" and vetor[4] == "preto":
+        if (vetor[0] <= 0.5 and (vetor[1] >= 0.75 and vetor[1] <= 1.25)) or vetor[2] == 4:
+            placa = 2
+    elif vetor[3] == "laranja" and vetor[4] == "preto":
+        if vetor[0] <= 0.5 or vetor[2] == 4:
+            placa = 4
+    else:
+        if vetor[0] <= 0.5 or vetor[2] == 4:
+            placa = 3
+            
+    return placa
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Pipeline de detecção de placas sem IA")
     parser.add_argument("imagem", help="Caminho para o arquivo de imagem")
